21fb9a78f85be91ac8d8230d8b8b1775b8f5bf5d
Vlajs <149913552+Vlajs@users.noreply.github.com>
2024-08-12 12:11:46 +0200

    Added newMethod3 method

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -42,6 +42,9 @@ public class BasePage {
     public void newMethod(String text){
         System.out.println("This is new method which will accept only text.");
     }
+    public void newMethod3(String text3){
+        System.out.println("This is new method 3 which will accept only text.");
+    }
     public void writeText(By elementBy, String expectedText){
         waitVisibility(elementBy);
         driver.findElement(elementBy).clear();
